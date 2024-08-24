11ca5793a8302d41a46c0d9ae0df67e69058ccd7
tareqislam <blnre.tareq@gmail.com>
2024-08-24 20:01:12 +0600

    All files of the project done

---
+++ app/data.php
@@ -0,0 +1,180 @@
+<?php
+
+$about  = [
+
+"title"      => "About Our Company",
+"desc"       => "Partex Furniture Industries Limited is a leading furniture manufacturer, brought significant changes in the furniture industry of our country since its beginning.
+
+One of the largest Furniture manufacturer and retailer in Bangladesh a subsidiary company of Partex Star Group established in 1999 with a vision- to Make Partex Star Group a diversified, sustainable world class company to ensure superior customer satisfaction and stakeholders’ value.. ",
+"button"      => [
+
+
+               "title"    => "More Details",
+               "link"     => "#",
+               "bg_color" =>  "purple"
+],
+
+"photo"        =>   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7C4nQ0A92MYJT_478o0nR20zv4eOY-KYAOQ&s"
+
+
+
+
+];
+
+$slider =[
+
+
+[
+"title"        => "we love laravel",
+"desc"         => "This is slider section for arrays",
+"photo"        => "https://static.vecteezy.com/system/resources/thumbnails/028/558/024/small_2x/abstract-colorful-gradient-background-for-design-as-banner-ads-and-presentation-concept-photo.jpg",
+
+"button"       => [
+
+                 "btn1"  => [
+
+                "title"    => 'Read More',
+                "link"     => "#"
+
+                 ],
+                 "btn2"  => [
+
+                 "title"    => 'Apply Now',
+                 "link"     => "#"
+
+                 ]
+
+
+
+]
+
+
+],
+
+[
+    "title"        => "we love Laravel",
+    "desc"         => "This is slider section for laravel",
+    "photo"        => "https://images.pexels.com/photos/573130/pexels-photo-573130.jpeg?cs=srgb&dl=pexels-zyuliansyah-573130.jpg&fm=jpg",
+
+    "button"       => [
+    
+                     "btn1"  => [
+    
+                    "title"    => 'Read More',
+                    "link"     => "#"
+    
+                     ],
+                     "btn2"  => [
+    
+                     "title"    => 'Apply Now',
+                     "link"     => "#"
+    
+                     ]
+    
+    
+    
+    ]
+    
+    
+    ],
+
+    [
+        "title"        => "we love SPSS",
+        "desc"         => "This is slider section for SPSS",
+        "photo"        => "https://zeevector.com/wp-content/uploads/Youtube-Banner-Background.png",
+    
+        "button"       => [
+        
+                         "btn1"  => [
+        
+                        "title"    => 'Read More',
+                        "link"     => "#"
+        
+                         ],
+                         "btn2"  => [
+        
+                         "title"    => 'Apply Now',
+                         "link"     => "#"
+        
+                         ]
+        
+        
+        
+        ]
+        
+        
+        ]
+    
+
+
+];
+
+
+$blog  =[
+
+"title"             => "Blog About Partex",
+
+"blog_1"           =>[
+
+
+                  "bg_photo"   => "https://5.imimg.com/data5/JU/ZX/BR/ANDROID-43658668/product-jpeg-500x500.jpg",
+
+                  "title"      =>  "<h5>SOFA SET 6 For Sale</h5>",
+                  "desc"       => "<p>Displayed fabric color/design may change due to availability of stock. If fabric is not available (shown color/design)  you need to choose from our running available fabric option.Displayed fabric color/design may change due to availability of stock. If fab</p>",
+
+                  "button"     => [
+
+                            "title"    => "Read Details",
+                            "link"      => "#",
+                            "bg_color"  =>  "Red"
+
+                  ]
+                
+                  ],
+
+"blog_2"           =>[
+
+
+                  "bg_photo"   => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3s_A8E-XTsX9VIcmuOiILuMYQQj6zaODtFw&s",
+
+                  "title"      =>  "<h5>FurnitureHouse Wooden L Shape Sofa Set, For Home, Living Room</h5>",
+                  "desc"       => "<p>Displayed fabric color/design may change due to availability of stock. If fabric is not available (shown color/design)  you need to choose from our running available fabric option.Displayed fabric color/design may change due to availability of stock. If fab</p>",
+
+                  "button"     => [
+
+                            "title"    => "Read More",
+                            "link"      => "#",
+                            "bg_color"  =>  "Green"
+
+                  ]
+                
+                  ],
+
+        "blog_3"           =>[
+
+
+                  "bg_photo"   => "https://5.imimg.com/data5/JU/ZX/BR/ANDROID-43658668/product-jpeg-500x500.jpg",
+
+                  "title"      =>  "<h5>Queen size bed 0197 -WF MG-01 </h5>",
+                  "desc"       => "<p>Exclusive Design Leg & Head side on well-seasoned 12-15 % moisture controlled and chemical treated mahogany wood Displayed fabric color/design may change due to availability of stock. This is some good stocks of furniture.
+.</p>",
+
+                  "button"     => [
+
+                            "title"    => "Apply Now",
+                            "link"      => "#",
+                            "bg_color"  =>  "Blue"
+
+                  ]
+                
+                  ],
+        
+
+
+
+];
+
+
+
+
+?>
